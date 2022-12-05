f4f7b0fa459ab610e1f2226e849a0d8e71b1d336
Abhiram Pai <abhirampai1999@gmail.com>
2022-12-05 11:37:58 +0530

    Create day5secondpuzzle.py

---
+++ day5secondpuzzle.py
@@ -0,0 +1,18 @@
+sample_crate_stacks = [["N", "Z"], ["D", "C", "M"], ["P"]]
+
+import re
+
+def pop_and_insert(count, src, dest):
+  for i in range(0, count):
+    element_to_be_inserted = crate_stacks[src - 1].pop(0)
+    crate_stacks[dest - 1].insert(i, element_to_be_inserted)
+    
+for instruction in instructions.split("\n"):
+  count, src, dest = list(map(int, re.findall(r'\d+', instruction)))
+  pop_and_insert(count, src, dest)
+
+output = ""
+for stacks in crate_stacks:
+  output += stacks[0]
+
+print(output)
